80569e051f1fdfebc1eed4134622ad0a19775f22
Burt Beckwith <beckwithb@vmware.com>
2012-12-15 20:57:07 -0500

    added release plugin, and also needed Hibernate plugin (or at least its deps) for SqlTiming

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,6 +13,11 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
+        build(':release:2.2.0', ':rest-client-builder:1.0.3') {
+            export = false
+        }
+
+        runtime ":hibernate:$grailsVersion"
         compile ':profiler:0.4'
     }
 }
