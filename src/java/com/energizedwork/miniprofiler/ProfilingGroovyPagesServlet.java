3cb7b33498382543d6128380496db8ab12df0a2f
Burt Beckwith <beckwithb@vmware.com>
2012-12-15 21:05:01 -0500

    the signature of GroovyPagesServlet.renderPageWithEngine changed, so the code wasn't compiling in 2.0+. If you convert to a Groovy class it will work in both 1.3 and 2.0+

--- src/java/com/energizedwork/miniprofiler/ProfilingGroovyPagesServlet.java
+++ src/java/com/energizedwork/miniprofiler/ProfilingGroovyPagesServlet.java
@@ -1,20 +1,37 @@
 package com.energizedwork.miniprofiler;
 
 import groovy.text.Template;
-import org.apache.commons.lang.StringUtils;
-import org.codehaus.groovy.grails.web.pages.GroovyPagesServlet;
-import org.codehaus.groovy.grails.web.pages.GroovyPagesTemplateEngine;
-import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes;
+
+import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.apache.commons.lang.StringUtils;
+import org.codehaus.groovy.grails.web.pages.GroovyPagesServlet;
+import org.codehaus.groovy.grails.web.pages.GroovyPagesTemplateEngine;
+import org.codehaus.groovy.grails.web.pages.discovery.GroovyPageScriptSource;
+import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes;
 
 public class ProfilingGroovyPagesServlet extends GroovyPagesServlet {
 
-    @Override
     protected void renderPageWithEngine(GroovyPagesTemplateEngine engine, HttpServletRequest request,
                                         HttpServletResponse response, Template template) throws IOException {
+
+        String pageName = determinePageName(request);
+//        System.out.println(pageName);
+//        super.renderPageWithEngine(engine, request, response, template);
+    }
+
+    protected void renderPageWithEngine(GroovyPagesTemplateEngine engine, HttpServletRequest request,
+                                        HttpServletResponse response, GroovyPageScriptSource scriptSource) throws Exception {
+
+        String pageName = determinePageName(request);
+//        System.out.println(pageName);
+        super.renderPageWithEngine(engine, request, response, scriptSource);
+    }
+
+    protected String determinePageName(HttpServletRequest request) {
 
         String pageName = (String)request.getAttribute(GrailsApplicationAttributes.GSP_TO_RENDER);
         if (StringUtils.isBlank(pageName)) {
@@ -25,9 +42,7 @@ public class ProfilingGroovyPagesServlet extends GroovyPagesServlet {
                 pageName = request.getServletPath();
             }
         }
-        System.out.println(pageName);
-        super.renderPageWithEngine(engine, request, response, template);
 
+        return pageName;
     }
-
 }
