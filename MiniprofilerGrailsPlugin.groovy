4a8b0b693ae091bf9b9f596aec5a795189abf88e
Tom Dunstan <github@tomd.cc>
2013-08-09 10:48:10 +0930

    Version 0.2

--- MiniprofilerGrailsPlugin.groovy
+++ MiniprofilerGrailsPlugin.groovy
@@ -19,7 +19,7 @@ import com.energizedwork.miniprofiler.ProfilingSpyLogDelegator
 import com.energizedwork.miniprofiler.ServletRequestProfilerProvider
 
 class MiniprofilerGrailsPlugin {
-    def version = "0.2-SNAPSHOT"
+    def version = "0.2"
     def grailsVersion = "1.3 > *"
 
     def loadAfter = ['profiler'] // as we want to modify a couple of things that it creates
