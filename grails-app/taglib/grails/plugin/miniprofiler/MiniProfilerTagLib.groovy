f661a46cb92d12e24eb647f57e15fafdd597d963
Damir Murat <damir.murat.git@gmail.com>
2013-10-13 12:04:02 +0200

    Expose and take into account various attributes supported by miniprofiler. Supply Ctrl+M as default keyboard shortcut.

--- grails-app/taglib/grails/plugin/miniprofiler/MiniProfilerTagLib.groovy
+++ grails-app/taglib/grails/plugin/miniprofiler/MiniProfilerTagLib.groovy
@@ -1,6 +1,7 @@
 package grails.plugin.miniprofiler
 
 import io.jdev.miniprofiler.json.ScriptTagWriter
+import org.codehaus.groovy.grails.web.taglib.GroovyPageAttributes
 
 class MiniProfilerTagLib {
 
@@ -9,9 +10,19 @@ class MiniProfilerTagLib {
     def profilerProvider
     def grailsApplication
 
-    def javascript = { attrs ->
-		if(!grailsApplication.config.grails?.profiler?.disable) {
-			out << new ScriptTagWriter().printScriptTag(profilerProvider.currentProfiler, "$request.contextPath/miniprofiler")
-		}
+    def javascript = { GroovyPageAttributes attrs ->
+        if(!grailsApplication.config.grails?.profiler?.disable) {
+            def convertedAttributes = [:]
+            convertedAttributes["data-position"] = attrs.get("data-position") ?: "left"
+            convertedAttributes["data-trivial"] = attrs.getBoolean("data-trivial") ?: false
+            convertedAttributes["data-children"] = attrs.getBoolean("data-children") ?: false
+            convertedAttributes["data-max-traces"] = attrs.getInt("data-max-traces") ?: 15
+            convertedAttributes["data-controls"] = attrs.getBoolean("data-controls") ?: false
+            convertedAttributes["data-authorized"] = attrs.getBoolean("data-authorized") ?: true
+            convertedAttributes["data-toggle-shortcut"] = attrs.get("data-toggle-shortcut") ?: "Ctrl+M"
+            convertedAttributes["data-start-hidden"] = attrs.getBoolean("data-start-hidden") ?: false
+
+            out << new ScriptTagWriter().printScriptTag(profilerProvider.currentProfiler, "$request.contextPath/miniprofiler", convertedAttributes)
+        }
     }
 }
