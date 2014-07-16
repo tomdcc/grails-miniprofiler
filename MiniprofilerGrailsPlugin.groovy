46ec1f372775b613e9ce42eed1f4af611c4b2b14
Tom Dunstan <tomdcc@users.noreply.github.com>
2014-07-16 13:37:54 +0930

    Version 0.4.1

--- MiniprofilerGrailsPlugin.groovy
+++ MiniprofilerGrailsPlugin.groovy
@@ -13,7 +13,7 @@ import grails.plugin.miniprofiler.profilerplugin.MiniprofilerCondition
 import org.springframework.cache.ehcache.EhCacheFactoryBean
 
 class MiniprofilerGrailsPlugin {
-    def version = "0.4.1-SNAPSHOT"
+    def version = "0.4.1"
     def grailsVersion = "1.3 > *"
 
     def loadAfter = ['profiler'] // as we want to modify a couple of things that it creates
